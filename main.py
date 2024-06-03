8ee74e53f00b37b829e06ce23f90ecb9bdf3c5cc
Utkarsh Vishwakarma <utkarshvishwakarma1411@gmail.com>
2024-06-04 00:42:21 +0530

    commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data=pd.read_csv('data.csv')
+
+plt.scatter(data.studytime, data.score)
+plt.show()
+
+
+
+def gradient_descent(m_now, b_now, points, L):
+    m_gradient = 0
+    b_gradient = 0
+
+    n = len(points)
+
+    for i in range(n):
+        x=points.iloc[i].studytime
+        y=points.iloc[i].score
+
+        m_gradient += -(2/n) * x * (y- (m_now * x) - b_now)
+        b_gradient += -(2 / n) * (y - (m_now * x) - b_now)
+    m = m_now - m_gradient * L
+    b = b_now - b_gradient * L
+    return m,b
+
+
+m=0
+b=0
+L = 0.001
+epochs = 100
+
+for i in range(epochs):
+    m,b = gradient_descent(m,b,data,L)
+
+print(m,b)
+plt.scatter(data.studytime, data.score, color = "black")
+plt.plot(list(range(1,12)), [m * x + b for x in range(1, 12)], color="red")
+plt.show()
